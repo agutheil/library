755da8b20d64bc9fe4d920b3f1574e4f235d8e99
agutheil <agu@foryouandyourcustomers.com>
2018-06-05 14:07:44 +0200

    tests build pipeline

--- src/main/java/com/fyayc/microprofile/repo/BookService.java
+++ src/main/java/com/fyayc/microprofile/repo/BookService.java
@@ -23,7 +23,7 @@ public class BookService {
     public BookService() {
         Book book = new Book();
         book.setId(getNextId());
-        book.setName("xxContinuous Delivery with Jenkins, GitHub, Docker and AWS ");
+        book.setName("Continuous Delivery with Jenkins, GitHub, Docker and AWS ");
         book.setIsbn("1");
         book.setAuthor("fyayc");
         book.setPages(420);
